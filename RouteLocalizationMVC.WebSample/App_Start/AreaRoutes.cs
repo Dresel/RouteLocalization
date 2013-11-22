966da7a2e90c9f7d5779a5b1bd855ef2b212d72b
Christopher Dresel <christopher.dresel@fhwn.ac.at>
2013-11-22 11:08:18 +0100

    Restructured extension method structure

---
+++ RouteLocalizationMVC.WebSample/App_Start/AreaRoutes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RouteLocalizationMVC.WebSample.Areas.Area.Controllers;
+
+namespace RouteLocalizationMVC.WebSample.App_Start
+{
+	public static class AreaRoutes
+	{
+		public static void AddAreaRoutesTranslation(this Localization localization)
+		{
+			localization.ForCulture("de")
+				.SetAreaPrefix("Area")
+				.ForController<HomeController>()
+				.ForAction(x => x.Index())
+					.AddTranslation("Willkommen/{foo}")
+				.ForAction(x => x.Book())
+					.AddTranslation("Buch/{chapter}/{page}/{foo}");
+		}
+	}
+}
---
+++ RouteLocalizationMVC.WebSample/App_Start/DefaultRoutes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RouteLocalizationMVC.WebSample.Controllers;
+
+namespace RouteLocalizationMVC.WebSample.App_Start
+{
+	public static class DefaultRoutes
+	{
+		public static void AddDefaultRoutesTranslation(this Localization localization)
+		{
+			localization.ForCulture("de")
+				.ForController<HomeController>()
+				.ForAction(x => x.Index())
+					.AddTranslation("Willkommen")
+				.ForAction(x => x.Book())
+					.AddTranslation("Buch/{chapter}/{page}");
+
+			localization.ForCulture("de")
+				.ForController<HomeWithRoutePrefixAttributeController>()
+				.SetRoutePrefix("RoutePrefixDE")
+					.ForAction(x => x.Index())
+						.AddTranslation("Willkommen")
+					.ForAction(x => x.Book())
+						.AddTranslation("Buch/{chapter}/{page}");
+
+			localization.ForCulture("de")
+				.SetAreaPrefix("AreaPrefixDE")
+				.ForController<HomeWithRouteAreaAttributeController>()
+					.ForAction(x => x.Index())
+						.AddTranslation("Willkommen")
+					.ForAction(x => x.Book())
+						.AddTranslation("Buch/{chapter}/{page}");
+		}
+	}
+}
