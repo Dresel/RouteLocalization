d51e50930d2828ea844e535deddf65c5438f2504
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-06-05 17:42:25 +0200

    Uncomment default routes

--- src/RouteLocalization.AspNetCore.Web/Startup.cs
+++ src/RouteLocalization.AspNetCore.Web/Startup.cs
@@ -40,7 +40,7 @@
 
 			app.UseStaticFiles();
 
-			app.UseMvc(routes => { /*routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");*/ });
+			app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"); });
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
