a2c6cd7db032642091ba491c89b529ac4d147216
Christopher Dresel <christopher.dresel@spatial-focus.net>
2017-04-18 08:02:09 +0200

    Wipe master

--- Source/RouteLocalization.Http.Nuget/make.bat
+++
@@ -1,7 +0,0 @@
-mkdir input\lib\net40
-del /Q input\lib\net40\*.*
-
-msbuild .\..\RouteLocalization.Http\RouteLocalization.Http.csproj /p:Configuration=Release;DefineConstants="ASPNETWEBAPI";OutputPath=.\..\RouteLocalization.Http.Nuget\input\lib\net40
-
-mkdir output
-..\.nuget\nuget.exe pack /o output .\RouteLocalization.WebApi.nuspec
